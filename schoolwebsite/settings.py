691706e78180d6c42724f39ab899c258f9bbffef
Toluwashe Samuel <timijomi@gmail.com>
2025-12-01 10:21:44 +0100

    Fix start command

--- schoolwebsite/settings.py
+++ schoolwebsite/settings.py
@@ -144,15 +144,14 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # --- Email Configuration (Render + Brevo) ---
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-
-# --- Email Configuration (Render) ---
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"        # or smtp.office365.com, smtp.sendgrid.net, etc.
+EMAIL_HOST = "smtp-relay.brevo.com"       # or smtp.gmail.com, smtp.office365.com
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 
 
